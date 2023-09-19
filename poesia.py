1e6bc86db3dd8193d29cb8a61186532cead4e936
Patricia Angelini <pf1076@fiap.com.br>
2023-09-19 02:00:08 -0300

    adicionado poesia.py

---
+++ poesia.py
@@ -0,0 +1,11 @@
+print("Antes, e com tal zelo, e sempre, e tanto......")
+print("Que mesmo em face do maior encanto")
+print("Dele se encante mais meu pensamento!")
+print("\n\n\n")
+
+print("Quero vivê-lo em cada vão momento.")
+print("E em seu louvor hei de espalhar meu canto.")
+print("E rir meu riso e derramar meu pranto.")
+print("Ao seu pesar ou seu contentamento")
+print("\n\n\n")
+
